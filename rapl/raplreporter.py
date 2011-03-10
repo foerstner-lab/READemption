47236dbcbc47e35058c6ea94cb8e40c2e7cbf628
Konrad Förstner <konrad@foerstner.org>
2011-03-10 18:34:07 +0100

    Improving the reporter output

--- rapl/raplreporter.py
+++ rapl/raplreporter.py
@@ -17,19 +17,36 @@ class RaplReporter(object):
         template = Template(self._template_text())
         return(template.substitute(
               date = datetime.today().strftime("%d.%m.%Y"),
-              read_lib_listing = self._read_lib_listing()
-                ))
+              read_lib_listing = self._read_lib_listing(),
+              genome_file_listing = self._genome_file_listing(),
+              min_seq_length = self.rapl_instance.min_seq_length,
+              max_a_content = self.rapl_instance.max_a_content,
+              read_mapping_folder = self._latex_safe(
+                    self.rapl_instance.read_mapping_folder),
+              unmapped_read_first_run_folder = self._latex_safe(
+                self.rapl_instance.umapped_reads_of_first_mapping_folder)))
 
     def _read_lib_listing(self):
         """ """
         listing = "\\begin{itemize}\n"
         for read_file in self.rapl_instance.read_files:
             listing += "\\item %s with %s reads " % (
-                read_file.replace("_", ""),
+                self._latex_safe(read_file),
                 self.rapl_instance._number_of_fasta_entries(
                     self.rapl_instance._read_file_path(read_file)))
         listing += "\\end{itemize}\n"
         return(listing)
+
+    def _genome_file_listing(self):
+        listing = "\\begin{itemize}\n"
+        listing += "\n".join(
+            ["\\item %s\n" % self._latex_safe(genome_file)
+             for genome_file in self.rapl_instance.genome_files])
+        listing += "\\end{itemize}\n"
+        return(listing)
+
+    def _latex_safe(self, input_string):
+        return(input_string.replace("_", "\_"))
 
     def _template_text(self):
         return("""\\documentclass[12pt,a4paper,oneside]{article}
@@ -48,14 +65,14 @@ class RaplReporter(object):
 
 \\section{Introduction}
 
-This report intends to help you understanding the results of a Solexa
-or 454 sequncing and analyzing process. 
+This report intends to help you to understand the results of a Solexa
+or 454 sequncing and analyzing process.
 
 \\section{Used libraries}
 
 The first step of the process is the actual sequencing of the cDNA
-libraries. The result of this sequencing is a set of FASTA file
-containing the raw reads. For your project we used the following read
+libraries. The result of this sequencing is a set of FASTA files
+which contain the raw reads. For your project we used the following read
 files:
 
 $read_lib_listing
@@ -66,54 +83,61 @@ $read_lib_listing
 
 The mapping against the reference genome was done using the program
 \\texttt{segemehl} (Hoffmann \\textit{et. al}, \\textit{PLoS
-  Computational Biology}, 2009) and in two steps. In the first step it
+Computational Biology}, 2009) and in two steps. In the first step it
 was tried to align all raw reads to the reference genome. Usually this
 is not successful for all of them e.g due to sequencing errors. Some
 read cannot be mapped as they contain a poly-A tail. To tackle this
-problem all read for which the mapping failed in the first run were
+problem all reads for which the mapping failed in the first run were
 undergoing a procedure in which a potential poly-A tail was searched
 and clipped. After the clipping the resulting sequences were filtered
-by sequence size: All sequences that were shorter than 12 nucleotides
-were removed. The other remaining reads were aligned to the genome in
-a second round. After this mapping results were combined. These
-combined entries were then filtered by the A-content. All entries with
-more than 70\\% of A's were removed. These cleaned results are then
-split by the genome files (of chromosome and/or plasmids) they are
-mapped to.\\\\
+by sequence size: All sequences that were shorter than $min_seq_length
+nucleotides were removed. The other remaining reads were aligned to
+the genome in a second round. After this mapping results were
+combined. These combined entries were then filtered by the
+A-content. All entries with more than $max_a_content\\% of A's were
+removed. These cleaned results are then split by the genome files (of
+chromosome and/or plasmids) they are mapped to.\\\\
 
 If you look at the mapping results you might noticed that some reads
 are listed more than once. We allowed only one best \\texttt{segemehl}
 hit per read but for certain reads there were some hits sharing the
 same best value. In such case two or more placements are accepted.
 
-% \\subsection{Used Genome files}
+\\subsection{Used Genome files}
 
-% The following genome files were used as reference
-% set:
+The following genome files were used as reference
+set:
 
-% \\texttt{segemehl} generates on combined index files of them.
+$genome_file_listing
 
-% \\subsection{First read mapping}
+\\texttt{segemehl} generates one combined index files of them.
 
-% The output file of the first mapping can be found in the folder XXX:
+\\subsection{First read mapping}
 
-% \\subsection{Poly-A clipping}
+The output files of the first mapping can be found in the subfolder
+named \\textit{$read_mapping_folder}.
 
-% The output file of the first mapping can be found in the folder XXX:
+\\subsection{Unmapped reads, poly-A clipping and size filtering}
 
-% \\subsection{Size filtering}
+The files which contain read that were not mapped in the first run are
+put in the folter \\textit{$unmapped_read_first_run_folder}. The
+clipped files based on these files are also there. Likewise the files
+that contain the reads which are equal or greate the given cut-off
+(they have a "gtoe" in the file name) and the file that contains the
+reads that are too small after the clipping ((they have a "lt" in the
+file name)).
 
-% \\subsection{Second read mapping}
+\\subsection{Second read mapping}
 
-% \\subsection{Combining of the mapping results}
+\\subsection{Combining of the mapping results}
 
-% \\subsection{Filtering the results by A-content
+\\subsection{Filtering the results by A-content}
 
-% \\subsection{Tracing files}
+\\subsection{Tracing files}
 
-% \\subsection{Splitting result by genome file}
+\\subsection{Splitting result by genome file}
 
-% \\section{Preparing for visualization}
+\\section{Preparing for visualization}
 
 To visualize and compare the different libraries a genome browser can
 be used. Usually \\texttt{Integrated Genome Browser} (IGB) is a
