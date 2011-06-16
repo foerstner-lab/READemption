d70dff462e6e440191dfb6bf7acdfc5901e4b898
Konrad Förstner <konrad@foerstner.org>
2011-06-16 19:36:55 +0200

    Remove nucleotide normalization related code

--- rapl/grbuilder.py
+++ rapl/grbuilder.py
@@ -86,39 +86,3 @@ class GrBuilder(object):
         return(
             sam_parser.number_of_mapped_reads(
                 self.paths.combined_mapping_file_a_filtered(read_file)))
-    
-    # def _count_mapped_nucleotides(self, read_file, genome_file):
-    #     """Count number of successfully mapped reads.
-
-    #     Arguments:
-    #     - `read_file`: orignal read file used to generate the mappings.
-    #     - `genome_file`: targe genome file
-
-    #     """
-    #     sam_parser = SamParser()
-    #     nucleotide_counting = 0
-    #     prev_entry = ""
-    #     for entry in sam_parser.entries(
-    #         self.paths.combined_mapping_file_a_filtered_split(
-    #             read_file, genome_file)):
-    #         if entry['query'] == prev_entry:
-    #             continue
-    #         nucleotide_counting += len(entry['sequence'])
-    #         prev_entry = entry['query']
-    #     return(nucleotide_counting)
-
-    # def _lowest_number_of_mapped_nucleotides(self, genome_file):
-    #     """Return the lowest number of mapping mapped nucleotides.
-
-    #     Arguments:
-    #     - `genome_file`: target genome file
-
-    #     """
-    #     lowest_number_of_mapped_nucleotides = min(
-    #         [self._count_mapped_nucleotides(read_file, genome_file) 
-    #          for read_file in self.paths.read_files])
-    #     # Do avoid multiplication by zero
-    #     if lowest_number_of_mapped_nucleotides == 0:
-    #         lowest_number_of_nucleotides = 1
-    #     return(lowest_number_of_mapped_nucleotides)
-
