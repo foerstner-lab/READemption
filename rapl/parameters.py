96bb43859def12f516fb073f9c72e7ab9df04865
Konrad Förstner <konrad@foerstner.org>
2011-06-28 08:11:54 +0200

    Add exception handling parameter.

--- rapl/parameters.py
+++ rapl/parameters.py
@@ -3,8 +3,16 @@ class Parameters(object):
     segemehl_hit_strategy = "1"
     segemehl_max_e_value = 5.0
     segemehl_number_of_threads = 1
-    python_number_of_threads = 1
+    python_number_of_threads = 3
+
+    # This define how expection should be treated exspecilly the once
+    # that run in parellel. The option are "report" or "crash"
+    # - report: The exceptions are written to stderr but the program
+    #           should continue
+    # - crash: the exception is raised
+    exception_handling = "report"
     # Filtering
     min_seq_length = 12
     max_a_content = 70.0
     min_overlap = 1
+    min_read_overlap_perc = 0.0
