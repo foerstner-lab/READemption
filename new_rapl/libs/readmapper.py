b9103c762552e84119ec2f8815386db93be95006
Konrad Förstner <konrad@foerstner.org>
2011-09-13 17:19:53 +0200

    Finish ReadMapper

--- new_rapl/libs/readmapper.py
+++ new_rapl/libs/readmapper.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append(".")
-from segemehl import Segemehl
+from libs.segemehl import Segemehl
 
 class ReadMapper(object):
 
@@ -8,20 +8,14 @@ class ReadMapper(object):
         self.segemehl = Segemehl()
     
     def build_index(self, genome_file_paths, index_file_path):
-        """
-        
-        Arguments:
-        - `genome_file_paths`:
-        - `index_file_path`:
-        """
         self.segemehl.build_index(genome_file_paths, index_file_path)
         
     def run_mappings(self, read_file_paths, genome_file_paths, 
                      index_file_path, output_file_paths, nomatch_file_paths,
-                     treads, accuracy):
+                     threads, accuracy):
         for read_file_path, output_file_path, nomatch_file_path in zip(
-            read_file_paths, output_file_paths, nomatch_file_paths)
+            read_file_paths, output_file_paths, nomatch_file_paths):
             self.segemehl.map_reads(
                 read_file_path, index_file_path, genome_file_paths, 
                 output_file_path, nonmatch_file=nomatch_file_path, 
-                treads=treads, accuracy=accuracy)
+                threads=threads, accuracy=accuracy)
