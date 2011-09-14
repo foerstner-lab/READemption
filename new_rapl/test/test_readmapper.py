76c0df2aa16a8a84d313c8cb3e79cdca7159fceb
Konrad Förstner <konrad@foerstner.org>
2011-09-14 13:20:30 +0200

    Start TestReadMapper

---
+++ new_rapl/test/test_readmapper.py
@@ -0,0 +1,19 @@
+import unittest
+import sys
+from io import StringIO
+sys.path.append(".")
+from libs.readmapper import ReadMapper
+
+class TestReadMapper(unittest.TestCase):
+
+    def setUp(self):
+        self.read_mapper = ReadMapper()
+    
+    def test_build_index(self):
+        self.build_index()
+
+    def test_run_mappings(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
