7b585308d8d28309a03d8ecfb39b5125bc99208b
Konrad Förstner <konrad@foerstner.org>
2012-06-26 09:00:15 +0200

    Improve call in segemehl.py

--- new_rapl/libs/segemehl.py
+++ new_rapl/libs/segemehl.py
@@ -9,26 +9,29 @@ class Segemehl(object):
 
     def build_index(self, fasta_files, index_file):
         """Create an index based on a list of fasta files"""
-        call("%s --database %s --generate %s " % 
-             (self.segemehl_bin, " ".join(fasta_files), index_file),
-             shell=True)
+        call([self.segemehl_bin, "--database"] + fasta_files + [
+             "--generate", index_file])
 
     def map_reads(self, read_file, index_file, fasta_files, output_file,
                   hit_strategy=1, accuracy=95, evalue=5.0, threads=1,
                   segemehl_format=False, order=False, nonmatch_file=None,
                   other_parameters=None):
-        segemehl_call = (
-            "%s --query %s --index %s --database %s --outfile %s " 
-            "--hitstrategy %s --accuracy %s --evalue %s --threads %s"
-            % (self.segemehl_bin, read_file, index_file, " ".join(fasta_files),
-               output_file, hit_strategy, accuracy, evalue, threads))
+        segemehl_call = [
+            self.segemehl_bin, "--query", read_file,
+            "--index", index_file,
+            "--database"] + fasta_files + [
+            "--outfile", output_file, 
+            "--hitstrategy", str(hit_strategy),
+            "--accuracy", str(accuracy),
+            "--evalue", str(evalue),
+            "--threads", str(threads)]
         if segemehl_format:
-            segemehl_call += " --SEGEMEHL"
+            segemehl_call.append("--SEGEMEHL")
         if order:
-            segemehl_call += " --order"
+            segemehl_call.append("--order")
         if nonmatch_file:
-            segemehl_call += " --nomatchfilename %s" % nonmatch_file
+            segemehl_call += ["--nomatchfilename", nonmatch_file]
         if other_parameters:
-            segemehl_call += " " + other_parameters
-        call(segemehl_call, shell=True)
+            segemehl_call.append(other_parameters)
+        call(segemehl_call)
 
