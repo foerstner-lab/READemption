51ca1403a9d3a62dde30f2c8f97df8caba9a4eb6
Konrad Förstner <konrad@foerstner.org>
2013-02-05 21:12:35 +0100

    Restructure CoverageCreator

--- rapl/libs/coveragecreator.py
+++ rapl/libs/coveragecreator.py
@@ -51,22 +51,27 @@ class CoverageCreator(object):
             self._write_to_wiggle_files(
                 output_file_prefix, read_file_name, factor=factor)
 
-    def _write_to_wiggle_files(self, output_file_prefix, read_file_name, factor):
+    def _write_to_wiggle_files(
+            self, output_file_prefix, read_file_name, factor):
         for strand in ["plus", "minus"]:
             output_fh = open("%s_%s.wig" % (output_file_prefix, strand), "w")
-            output_fh.write("track type=wiggle_0 name=\"%s_%s\"\n" % (
-                    read_file_name, strand))
-            for element in sorted(self.elements_and_coverages[strand].keys()):
-                output_fh.write("variableStep chrom=%s span=1\n" % (element))
-                # Filter values of 0 and multiply other the remaining
-                # ones by the given factor. pos is increased by 1 as a
-                # translation from a 0-based sysem (Python list) to a
-                # 1 based system (wiggle) takes place.
-                output_fh.write(
-                    "\n".join(
-                        ["%s %s" % (pos + 1, coverage * factor)
-                         for pos, coverage in
-                         filter(lambda pos_and_cov: pos_and_cov[1] != 0.0,
-                                enumerate(self.elements_and_coverages[
-                                        strand][element]))]) + "\n")
+            self._write_to_wiggle_file(
+                output_fh, read_file_name, factor, strand)
             output_fh.close()
+
+    def _write_to_wiggle_file(self, output_fh, read_file_name, factor, strand):
+        output_fh.write("track type=wiggle_0 name=\"%s_%s\"\n" % (
+            read_file_name, strand))
+        for element in sorted(self.elements_and_coverages[strand].keys()):
+            output_fh.write("variableStep chrom=%s span=1\n" % (element))
+            # Filter values of 0 and multiply other the remaining
+            # ones by the given factor. pos is increased by 1 as a
+            # translation from a 0-based sysem (Python list) to a
+            # 1 based system (wiggle) takes place.
+            output_fh.write(
+                "\n".join(
+                    ["%s %s" % (pos + 1, coverage * factor)
+                     for pos, coverage in
+                     filter(lambda pos_and_cov: pos_and_cov[1] != 0.0,
+                            enumerate(self.elements_and_coverages[
+                                strand][element]))]) + "\n")
