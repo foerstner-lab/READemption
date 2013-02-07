91209a7841f34f6676ca589e704659684bb69a94
Konrad Förstner <konrad@foerstner.org>
2013-02-07 11:33:10 +0100

    Add ReadProcessor

---
+++ rapl/libs/readprocessor.py
@@ -0,0 +1,43 @@
+from libs.fasta import FastaParser
+from libs.polyaclipper import PolyAClipper
+
+class ReadProcessor(object):
+
+    def __init__(self, poly_clipping=True,  min_read_length=12):
+        self.poly_clipping = poly_clipping
+        self._min_read_length = min_read_length
+        self.fasta_parser = FastaParser()
+        self.poly_a_clipper = PolyAClipper()
+
+    def process(self, input_path, output_path):
+        self._stats = {
+            "total_no_of_reads" : 0,
+            "polya_removed" : 0,
+            "single_a_removed" : 0,
+            "unmodified" : 0,
+            "too_short" : 0,
+            "long_enough" : 0}
+        output_fh = open(output_path, "w")
+        self._process(open(input_path), output_fh)
+        output_fh.close()
+        return(self._stats)
+
+    def _process(self, input_fh, output_fh):
+        for header, seq in self.fasta_parser.entries(input_fh):
+            self._stats["total_no_of_reads"] += 1
+            if self.poly_clipping:
+                clipped_seq = self.poly_a_clipper.clip_poly_a_strech(seq)
+                clipped_seq = self.poly_a_clipper.remove_3_prime_a(clipped_seq)
+            else:
+                clipped_seq = seq
+            if len(clipped_seq) == len(seq) - 1:
+                self._stats["single_a_removed"] += 1
+            elif len(clipped_seq) < len(seq) - 1:
+                self._stats["polya_removed"] += 1
+            else:
+                self._stats["unmodified"] += 1
+            if len(clipped_seq) < self._min_read_length:
+                self._stats["too_short"] += 1
+                continue
+            self._stats["long_enough"] += 1
+            output_fh.write(">%s\n%s\n" % (header, clipped_seq))
