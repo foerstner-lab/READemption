360bd61012373209716b2f17119aed0798166922
Konrad Förstner <konrad@foerstner.org>
2013-11-07 08:12:48 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='trapl',
+    version='0.1.6',
+    packages=['trapllib', 'tests'],
+    author='Konrad U. Förstner',
+    author_email='konrad@foerstner.org',
+    description='The RNA-Seq Analysis Pipeline',
+    url='',
+    install_requires=[
+        "pysam >= 0.7.6"
+    ],
+    scripts=['bin/trapl'],
+    license='LICENSE.txt',
+    long_description=open('README.txt').read(),
+    classifiers=[
+        'License :: OSI Approved :: ISC License',
+        'Programming Language :: Python :: 3',
+    ]
+)
