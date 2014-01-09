ea4391addeab65c28c9eef2f1c724c249ab4048c
Konrad Förstner <konrad@foerstner.org>
2014-01-09 12:21:18 +0100

    Fix path tests

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -8,14 +8,15 @@ from trapllib.paths import Paths
 class TestPaths(unittest.TestCase):
 
     test_folder = "/tmp/test"
-    test_files = ["foo", "bar"]
+    test_files = ["foo.fa", "bar.fa"]
+    test_lib_names = ["foo", "bar"]
 
     def setUp(self):
         self.paths = Paths(base_path=self.test_folder)
         self.folder_names = [
             self.paths.input_folder,
             self.paths.output_folder,
-            self.paths.report_folder,
+            self.paths.align_report_folder,
             self.paths.raw_stat_data_folder,
             self.paths.read_fasta_folder,
             self.paths.ref_seq_folder,
@@ -27,7 +28,7 @@ class TestPaths(unittest.TestCase):
             self.paths.coverage_raw_folder,
             self.paths.coverage_tnoar_min_norm_folder,
             self.paths.coverage_tnoar_mil_norm_folder,
-            self.paths.gene_quanti_folder,
+            self.paths.gene_quanti_base_folder,
             self.paths.gene_wise_quanti_combined_path]
         self.static_files = [
             self.paths.read_processing_stats_path,
@@ -35,7 +36,6 @@ class TestPaths(unittest.TestCase):
             self.paths.read_file_stats,
             self.paths.read_alignment_stats_table_path,
             self.paths.ref_seq_file_stats,
-            self.paths.annotation_file_stats,
             self.paths.index_path]
 
     def tearDown(self):
@@ -60,37 +60,38 @@ class TestPaths(unittest.TestCase):
             self.assertEqual(self.static_files.count(file_name), 1)
 
     def test_get_sorted_folder_content(self):
-        self._create_folder_with_files(self.test_folder, self.test_files)
+        self._create_folder_with_files(self.test_folder, self.test_lib_names)
         self.assertEqual(
             self.paths._get_sorted_folder_content(self.test_folder),
-            sorted(self.test_files))
+            sorted(self.test_lib_names))
         self._remove_folder_if_exists(self.test_folder)
 
     def test_required_folders(self):
-        self.assertEqual(len(self.paths.required_folders()), 15)
+        self.assertEqual(len(self.paths.required_folders()), 25)
 
     def test_get_read_files(self):
         self.paths.read_fasta_folder = self.test_folder
-        self._create_folder_with_files(self.test_folder, self.test_files)
-        self.assertEqual(self.paths.get_read_files(), sorted(self.test_files))
+        self._create_folder_with_files(self.test_folder, self.test_lib_names)
+        self.assertEqual(self.paths.get_read_files(), sorted(self.test_lib_names))
         self._remove_folder_if_exists(self.test_folder)
 
     def test_get_ref_seq_files(self):
         self.paths.ref_seq_folder = self.test_folder
-        self._create_folder_with_files(self.test_folder, self.test_files)
+        self._create_folder_with_files(self.test_folder, self.test_lib_names)
         self.assertEqual(self.paths.get_ref_seq_files(),
-                         sorted(self.test_files))
+                         sorted(self.test_lib_names))
         self._remove_folder_if_exists(self.test_folder)
 
     def test_get_annotation_files(self):
         self.paths.annotation_folder = self.test_folder
-        self._create_folder_with_files(self.test_folder, self.test_files)
+        self._create_folder_with_files(self.test_folder, self.test_lib_names)
         self.assertEqual(self.paths.get_annotation_files(),
-                         sorted(self.test_files))
+                         sorted(self.test_lib_names))
         self._remove_folder_if_exists(self.test_folder)
 
     def test_set_read_files_dep_file_lists(self):
-        self.paths.set_read_files_dep_file_lists(self.test_files)
+        self.paths.set_read_files_dep_file_lists_single_end(
+            self.test_files, self.test_lib_names)
         for path_list in  [
             self.paths.read_paths, self.paths.processed_read_paths,
             self.paths.read_alignment_result_sam_paths,
@@ -99,13 +100,13 @@ class TestPaths(unittest.TestCase):
 
     def test_path_list_without_appendix(self):
         self.assertEqual(
-            self.paths._path_list(self.test_folder, self.test_files),
+            self.paths._path_list(self.test_folder, self.test_lib_names),
             ['/tmp/test/foo', '/tmp/test/bar'])
 
     def test_path_list_with_appendix(self):
         self.assertEqual(
             self.paths._path_list(
-                self.test_folder, self.test_files, appendix=".boing"),
+                self.test_folder, self.test_lib_names, appendix=".boing"),
             ['/tmp/test/foo.boing', '/tmp/test/bar.boing'])
 
     def test_set_ref_seq_paths(self):
