3aa2926ec8451a371f3b817d448f1831c09f2779
Konrad Förstner <konrad@foerstner.org>
2014-01-15 08:44:32 +0100

    Fix conversion of empty SAM to BAM files

--- trapllib/sambamconverter.py
+++ trapllib/sambamconverter.py
@@ -7,6 +7,12 @@ class SamToBamConverter(object):
         self._unsorted_appendix = ".tmp_unsorted"
 
     def sam_to_bam(self, sam_path, bam_path_prefix):
+        if self._sam_file_is_empty(sam_path) is True:
+            # pysam will generate an error if an emtpy SAM file will
+            # be converted. Due to this an empty bam file with the
+            # same header information will be generated from scratch
+            self._generate_empty_bam_file(sam_path, bam_path_prefix)
+            return
         temp_unsorted_bam_path = self._temp_unsorted_bam_path(
             bam_path_prefix)
         # Generate unsorted BAM file
@@ -25,3 +31,18 @@ class SamToBamConverter(object):
 
     def _temp_unsorted_bam_path(self, bam_path_prefix):
         return "%s%s.bam" % (bam_path_prefix, self._unsorted_appendix)
+
+    def _sam_file_is_empty(self, sam_path):
+        # Check if there is any line that is not a header line
+        # (i.e. which is not starting with @)
+        for line in open(sam_path):
+            if line.startswith("@") is False:
+                return False
+        return True
+
+    def _generate_empty_bam_file(self, sam_path, bam_path_prefix):
+        samfile = pysam.Samfile(sam_path, "r")
+        bamfile = pysam.Samfile(
+            "%s.bam" % bam_path_prefix, "wb", header=samfile.header)
+        bamfile.close()
+        samfile.close()
