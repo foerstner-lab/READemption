957223bce000173416ee91ee1d2d5467de5bd3ad
Konrad Förstner <konrad@foerstner.org>
2014-07-05 15:45:19 +0200

    Change example version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='READemption',
-    version='0.3.0',
+    version='0.3.1',
     packages=['reademptionlib', 'tests'],
     author='Konrad U. Förstner',
     author_email='konrad@foerstner.org',
