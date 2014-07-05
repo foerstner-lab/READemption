a2507162f3b9b6e021c4864e9ee49791692220af
Konrad Förstner <konrad@foerstner.org>
2014-07-05 11:15:43 +0200

    Add Fastq parser

---
+++ reademptionlib/fastq.py
@@ -0,0 +1,47 @@
+class FastqParser(object):
+    """A class for parsing and handling fastQ files. 
+
+    Currently only taking care of the sequences not the quality
+    strings. Only four line entries can be handled
+    """
+
+    def entries(self, fastq_fh):
+        """Go line by line though the file and return fastq entries. """
+        current_header = ''
+        current_sequence = ''
+        # Switch telling if this the first entry
+        virgin = True
+        entry_line_counter = 0
+        for line in fastq_fh:
+            # For usual headers
+            if line[0] == '@' and virgin == False and entry_line_counter == 4:
+                entry_line_counter = 1
+                yield(current_header, current_sequence)
+                current_header = line[1:-1]
+                current_sequence = ''
+            # For the very first header of the file
+            elif line[0] == '@' and virgin == True:
+                virgin = False
+                current_header = line[1:-1]
+                entry_line_counter = 1
+            # For sequence lines
+            else:
+                entry_line_counter += 1
+                if entry_line_counter == 2:
+                    current_sequence = line[:-1]
+        # For the last entry if file was not empty
+        if not (current_header == '' and current_sequence == ''):
+            yield(current_header, current_sequence)
+
+    def single_entry_file_header(self, fastq_fh):
+        first_line = fastq_fh.readline()
+        header = first_line[1:-1]
+        fastq_fh.seek(0)
+        return header
+
+    def header_id(self, header):
+        """Return only the id of a fastq header
+
+        Everything after the first white space is discard.
+        """
+        return header.split()[0]
---
+++ tests/test_fastq.py
@@ -0,0 +1,65 @@
+from io import StringIO
+import unittest
+import sys
+sys.path.append(".")
+from reademptionlib.fastq import FastqParser
+
+class TestFastqParser(unittest.TestCase):
+
+    def setUp(self):
+        self.fastq_parser = FastqParser()
+        self.example_data = ExampleData()
+
+    def test_parse_1(self):
+        fastq_fh = StringIO(self.example_data.fastq_seqs_1)
+        self.assertEqual(
+            list(self.fastq_parser.entries(fastq_fh)), 
+            [('test_1 a random sequence', 'TTTAGAAATTACACA'), 
+             ('test_2 another random sequence', 'ACGAGAAATTAAATTAAATT'), 
+             ('test_3 another random sequence', 'TAGAGACATTGGATTTTATT')])
+
+    def test_parse_empty_file(self):
+        fastq_fh = StringIO("")
+        self.assertEqual(
+            list(self.fastq_parser.entries(fastq_fh)), [])
+
+    def test_single_entry_file_header(self):
+        fastq_fh = StringIO(self.example_data.fastq_seqs_2)
+        self.assertEqual(self.fastq_parser.single_entry_file_header(fastq_fh), 
+                         "test_4 a random sequence")
+
+    def test_header_id_1(self):
+        self.assertEqual(
+            self.fastq_parser.header_id("seq_10101 An important protein"),
+            "seq_10101")
+
+    def test_header_id_2(self):
+        self.assertEqual(
+            self.fastq_parser.header_id("seq_10101\tAn important protein"),
+            "seq_10101")
+
+class ExampleData(object):
+
+    fastq_seqs_1 = """@test_1 a random sequence
+TTTAGAAATTACACA
++
+!!!!!!!!!!!!!!!
+@test_2 another random sequence
+ACGAGAAATTAAATTAAATT
++
+@@@@@@@@@@@@@@@@@@@@
+@test_3 another random sequence
+TAGAGACATTGGATTTTATT
++
+!!!!!!!!!!!!!!!!!!!!
+"""
+
+    fastq_seqs_2 = """@test_4 a random sequence
+TTTAGAAATTACACA
++
+!!!!!!!!!!!!!!!
+"""
+   
+if __name__ == "__main__":
+    unittest.main()
+    
