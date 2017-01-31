07063aa2f8865286d8398af4bfee9ef2239bc291
diarmaid <diarmaidtobin@web.de>
2017-01-31 18:33:51 +0100

    Increased functionality

--- reademptionlib/controller_tk.py
+++ reademptionlib/controller_tk.py
@@ -1,6 +1,9 @@
 import os
 import concurrent.futures
+from reademptionlib.helpers import Helpers
 from reademptionlib.paths import Paths
+from reademptionlib.readalignerstats import ReadAlignerStats
+from reademptionlib.rawstatdata import RawStatDataWriter
 from reademptionlib.wiggle import WiggleWriter
 
 
@@ -9,25 +12,73 @@ class Controller_TK(object):
     def __init__(self, args):
         """Create an instance."""
         self._args = args
-        self._paths = Paths
+        self._paths = Paths(args)
+        self._helpers = Helpers(args)
 
-    def viz_align_TK(self):
+    def generate_read_alignment_stats(
+            self, filenames, BAM_files, output_path, output_file):
+        """Manage the generation of alingment statistics."""
+        raw_stat_data_writer = RawStatDataWriter(pretty=True)
+        read_files_and_jobs = {}
+        with concurrent.futures.ProcessPoolExecutor(
+                max_workers=self._args.processes) as executor:
+            for (filename, BAM_file) in zip(
+                    filenames, BAM_files):
+                read_aligner_stats = ReadAlignerStats()
+                read_files_and_jobs[filename] = executor.submit(
+                    read_aligner_stats.count, BAM_files, "NA")
+        # Evaluate thread outcome
+        self._helpers.check_job_completeness(read_files_and_jobs.values())
+        read_files_and_stats = dict(
+            [(filename, job.result())
+             for filename, job in read_files_and_jobs.items()])
+        raw_stat_data_writer.write(
+            read_files_and_stats, output_file)
+
+    def viz_align_tk(self):
         """Generate plots based on the read processing and mapping"""
-        from reademptionlib.vizalign2 import AlignViz2
-        align_viz_tk = AlignViz2()
-        if self._args.input_align:
-            align_viz_tk.alignment_stats(
-                str(self._args.input_align),
-                str(self._args.output_align),
-                str(self._args.output_align))
-        if self._args.input_process:
-            align_viz_tk.process_stats(
-                str(self._args.input_process),
-                str(self._args.output_process),
-                str(self._args.output_process))
-        
+        from reademptionlib.vizalign import AlignViz
+        align_viz_tk = AlignViz()
+        if self._args.input_BAM:
+            for (input_file, output_file) in zip(
+                    self._args.input_BAM, self._args.output_BAM):
+                self.generate_read_alignment_stats(
+                    self.get_filename(
+                        input_file), input_file,
+                    os.path.dirname(output_file), output_file)
+        # Missing: the automated mapping process
+        if self._args.input_process and not self._args.input_align:
+            align_viz_tk.processing_viz(self._args.input_process,
+                                        self._args.output_path)
+        if self._args.input_align and not self._args.input_process:
+            align_viz_tk.alignment_viz(self._args.input_align,
+                                       self._args.output_path)
+        if self._args.input_process and self._args.input_align:
+            align_viz_tk.processing_viz(self._args.input_process,
+                                        self._args.output_path)
+            align_viz_tk.alignment_viz(self._args.input_align,
+                                       self._args.output_path)
+            align_viz_tk.alignment_processing_overview(
+                self._args.input_process, self._args.input_align,
+                self._args.output_path)
+
+    def viz_deseq_tk(self):
+        from reademptionlib.vizdeseq import DESeqViz
+        deseq_viz_tk = DESeqViz(self._args.input_file, self._args.output_path,
+                                self._args.padj_cutoff, self._args.condition,
+                                self._args.alpha, self._args.color_sig,
+                                self._args.color_non_sig, self._args.shape,
+                                self._args.glyph_size)
+        deseq_viz_tk.read_and_modificate_input()
+
+    def viz_gene_quanti_tk(self):
+        from reademptionlib.vizgenequanti import GeneQuantiViz
+        gene_quanti_viz_tk = GeneQuantiViz(self._args.input_file,
+                                           self._args.lib_names,
+                                           self._args.output_path)
+        gene_quanti_viz_tk.parse_input_table()
+
     def get_and_submit_bam_stats(self):
-        from reademptionlib.readalignerstats import ReadAlignerStats
         get_bam_stats = ReadAlignerStats()
         total_aligned_reads = []
         total_uniquely_aligned_reads = []
@@ -57,9 +108,9 @@ class Controller_TK(object):
             jobs.append(executor.submit(
                 self.create_coverage_files_TK, float(no_of_aligned_reads),
                 float(min_no_of_aligned_reads)))
-        self._check_job_completeness(jobs)
+        self._helpers.check_job_completeness(jobs)
 
-    def create_coverage_files_TK(
+    def create_coverage_files_tk(
             self, no_of_aligned_reads, min_no_of_aligned_reads):
         if not self._args.non_strand_specific:
             strands = ["forward", "reverse"]
@@ -141,12 +192,5 @@ class Controller_TK(object):
 
     def get_filename(self, path_file):
         return(os.path.splitext(os.path.basename(path_file))[0])
-
-    def _check_job_completeness(self, jobs):
-        """Check the completness of each job in a list"""
-        for job in concurrent.futures.as_completed(jobs):
-            if job.exception():
-                raise(job.exception())
-            
 
 
