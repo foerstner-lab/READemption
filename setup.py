63c022c5cca7d285e9f0b689aa897ee2a27e02c6
Konrad Förstner <konrad@foerstner.org>
2017-12-31 21:20:37 +0100

    Set version to 0.4.5
    (cherry picked from commit 3fecb49cdd38c2e96ea4707724c66d936be47cb6)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='READemption',
-    version='0.4.4',
+    version='0.4.5',
     packages=['reademptionlib', 'tests'],
     author='Konrad U. Förstner',
     author_email='konrad@foerstner.org',
