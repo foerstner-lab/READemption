c21f7a543070547fe31e3a53a5dac09f50f1abc1
Till Sauerwein <till.sauerwein@web.de>
2019-04-05 12:52:21 +0200

    Changed name ov version log file

--- reademptionlib/paths.py
+++ reademptionlib/paths.py
@@ -129,7 +129,7 @@ class Paths(object):
         self.deseq_tmp_session_info_script = "%s/tmp.R" % self.deseq_raw_folder
         self.deseq_session_info = "%s/R_session_info.txt" % (
             self.deseq_raw_folder)
-        self.version_path = "%s/versions_of_used_libraries.txt" % (
+        self.version_path = "%s/version_log.txt" % (
             self.align_report_folder)
 
     def _get_sorted_folder_content(self, folder):
