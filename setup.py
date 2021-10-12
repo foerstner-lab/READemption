a33c9bac36b765f79ec980c65308bb3857e65ae9
Tillsa <till.sauerwein@web.de>
2021-10-12 17:35:50 +0200

    Set version to 1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="READemption",
-    version="1.0.4",
+    version="1.0.5",
     packages=["reademptionlib", "tests"],
     author="Konrad U. Förstner, Till Sauerwein",
     author_email="konrad@foerstner.org",
