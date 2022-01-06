1aa37868c20668a0258e8ef29d4417c8a2010af8
Tillsa <till.sauerwein@web.de>
2022-01-06 12:52:40 +0100

    Added controller test for gene quanti (excluding cross mapped reads)

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -29,9 +29,26 @@ class ArgMock:
         self.min_phred_score = None
         self.adapter = None
         self.reverse_complement = False
+        self.no_count_split_by_alignment_no = False
+        self.no_count_splitting_by_gene_no = False
+        self.remove_cross_aligned_reads = True
+        self.non_strand_specific = False
+        self.min_overlap = 1
+        self.read_region = "global"
+        self.clip_length = 11
+        self.no_count_split_by_alignment_no = False
+        self.no_count_splitting_by_gene_no = False
+        self.add_antisense = False
+        self.antisense_only = False
+        self.non_strand_specific = False
+        self.allowed_features = None
+        self.unique_only = False
+        self.pseudocounts = False
+        self.check_for_existing_files = False
+        self.remove_cross_aligned_reads = True
         self.items = [self.project_path, self.species, self.min_read_length]
 
-    # make mock argument object iterable like argpars.Namespace()
+    # make mock argument object iterable like argparse.Namespace()
     def __iter__(self):
         return (i for i in ["project_path", "species", "min_read_length"])
 
@@ -48,6 +65,10 @@ class TestController(unittest.TestCase):
 
         self.triple_expected_after_alignment = "tests/test_files/reademption_analysis_triple_after_alignment"
         self.triple_expected_after_alignment_output = "tests/test_files/reademption_analysis_triple_after_alignment/output"
+        self.triple_expected_after_alignment_copy = "a_test_project"
+        self.triple_expected_after_alignment_copy_output = "a_test_project/output"
+        self.triple_expected_after_gene_quanti_output = "tests/test_files/reademption_analysis_triple_after_gene_quanti_remove_cross_aligned_reads/output"
+
 
     def tearDown(self):
         self._remove_triple_input_files_and_structure_copy()
@@ -59,9 +80,17 @@ class TestController(unittest.TestCase):
             self.triple_fresh_copy,
         )
 
+    def _copy_aligned_files_and_structure(self):
+        shutil.copytree(
+            self.triple_expected_after_alignment,
+            self.triple_expected_after_alignment_copy,
+        )
+
     def _remove_triple_input_files_and_structure_copy(self):
-        if os.path.exists(self.triple_fresh):
+        if os.path.exists(self.triple_fresh_copy):
             shutil.rmtree(self.triple_fresh_copy)
+        if os.path.exists(self.triple_expected_after_alignment_copy):
+            shutil.rmtree(self.triple_expected_after_alignment_copy)
 
     def _remove_project_folder(self):
         if os.path.exists(self.test_project_name):
@@ -104,6 +133,31 @@ class TestControllerReadAligning(TestController):
         assert expected_files_and_checksums == created_files_and_checksums
 
 
+class TestControllerGeneQuantification(TestController):
+    def test_gene_quantification(self):
+        self._version = 2.0
+        self._copy_aligned_files_and_structure()
+        self.controller.quantify_gene_wise()
+        # Collect the checksums of the created files
+        created_files_and_checksums = {}
+        for created_subfolder_files in os.walk(self.triple_expected_after_alignment_copy_output):
+            for created_subfolder_file in created_subfolder_files[2]:
+                output_file_path = created_subfolder_files[0] + "/" + created_subfolder_file
+                # exclude gz zipped files because of different checksums due to meta information
+                if not output_file_path.endswith(".gz"):
+                    hash = hashlib.md5(open(output_file_path,'rb').read()).hexdigest()
+                    created_files_and_checksums[created_subfolder_file] = hash
+        # Collect the checksums of the expected files
+        expected_files_and_checksums = {}
+        for expected_subfolder_files in os.walk(self.triple_expected_after_gene_quanti_output):
+            for expected_subfolder_file in expected_subfolder_files[2]:
+                output_file_path = expected_subfolder_files[0] + "/" + expected_subfolder_file
+                # exclude gz zipped files because of different checksums due to meta information
+                if not output_file_path.endswith(".gz"):
+                    hash = hashlib.md5(open(output_file_path,'rb').read()).hexdigest()
+                    expected_files_and_checksums[expected_subfolder_file] = hash
+        # Compare the checksums
+        assert expected_files_and_checksums == created_files_and_checksums
 
 if __name__ == "__main__":
     unittest.main()
