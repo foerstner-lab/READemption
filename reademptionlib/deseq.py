7aa929697c761955e5737b6d102c84bb045e059a
Tillsa <till.sauerwein@web.de>
2022-02-10 10:39:52 +0100

    Changes string formatting to f-strings

--- reademptionlib/deseq.py
+++ reademptionlib/deseq.py
@@ -45,35 +45,30 @@ class DESeqRunner(object):
         os.remove(self._deseq_tmp_session_info_script)
 
     def create_deseq_script_file(self):
+        #{'Co_culture_replicate_1': 'co_culture', 'Co_culture_replicate_2': 'co_culture', 'Co_culture_replicate_3': 'co_culture', 'Harboring_replicate_1': 'harboring', 'Harboring_replicate_2': 'harboring', 'Harboring_replicate_3': 'harboring', 'Infected_replicate_1': 'infected', 'Infected_replicate_2': 'infected', 'Infected_replicate_3': 'infected', 'Steady_state_replicate_1': 'steady_state', 'Steady_state_replicate_2': 'steady_state', 'Steady_state_replicate_3': 'steady_state', 'Uninfected_replicate_1': 'uninfected', 'Uninfected_replicate_2': 'uninfected', 'Uninfected_replicate_3': 'uninfected'}
         libs_to_conditions = dict(
             [
                 (lib, condition)
                 for lib, condition in zip(self._libs, self._conditions)
             ]
         )
+        #['Orientation of counted reads relative to the strand location of the annotation', 'Sequence name', 'Source', 'Feature', 'Start', 'End', 'Score', 'Strand', 'Frame', 'Attributes', 'Co_culture_replicate_1', 'Co_culture_replicate_2', 'Co_culture_replicate_3', 'Harboring_replicate_1', 'Harboring_replicate_2', 'Harboring_replicate_3', 'Infected_replicate_1', 'Infected_replicate_2', 'Infected_replicate_3', 'Steady_state_replicate_1', 'Steady_state_replicate_2', 'Steady_state_replicate_3', 'Uninfected_replicate_1', 'Uninfected_replicate_2', 'Uninfected_replicate_3']
         head_row = (
             open(self._gene_wise_quanti_combined_path)
             .readline()[:-1]
             .split("\t")
         )
+        #['Co_culture_replicate_1', 'Co_culture_replicate_2', 'Co_culture_replicate_3', 'Harboring_replicate_1', 'Harboring_replicate_2', 'Harboring_replicate_3', 'Infected_replicate_1', 'Infected_replicate_2', 'Infected_replicate_3', 'Steady_state_replicate_1', 'Steady_state_replicate_2', 'Steady_state_replicate_3', 'Uninfected_replicate_1', 'Uninfected_replicate_2', 'Uninfected_replicate_3']
         libs = head_row[self._first_data_column - 1 :]
-        libs_str = ",".join(["'%s'" % lib for lib in libs])
+        #'Co_culture_replicate_1','Co_culture_replicate_2','Co_culture_replicate_3','Harboring_replicate_1','Harboring_replicate_2','Harboring_replicate_3','Infected_replicate_1','Infected_replicate_2','Infected_replicate_3','Steady_state_replicate_1','Steady_state_replicate_2','Steady_state_replicate_3','Uninfected_replicate_1','Uninfected_replicate_2','Uninfected_replicate_3'
+        libs_str = ",".join([f"'{lib}'" for lib in libs])
         conditions = [libs_to_conditions[lib] for lib in libs]
-        condition_str = ", ".join(["'%s'" % cond for cond in conditions])
+        condition_str = ", ".join([f"'{cond}'" for cond in conditions])
         if not self._fc_shrinkage_off:
             beta_prior_str = ", betaPrior=TRUE"
         elif self._fc_shrinkage_off:
             beta_prior_str = ""
-        file_content = self._deseq_script_template() % (
-            self._gene_wise_quanti_combined_path,
-            self._first_data_column - 1,
-            len(libs),
-            self._first_data_column,
-            libs_str,
-            condition_str,
-            beta_prior_str,
-            self._deseq_pca_heatmap_path,
-        )
+        file_content = self._deseq_script_template(len(libs), libs_str, condition_str, beta_prior_str)
         file_content += self._comparison_call_strings(conditions)
         deseq_fh = open(self._deseq_script_path, "w")
         deseq_fh.write(file_content)
@@ -84,30 +79,22 @@ class DESeqRunner(object):
 
     def merge_counting_files_with_results(self):
         for comparison_file, combo in self._comparison_files_and_combos:
-            output_fh = open(
-                "%s/%s"
-                % (
-                    self._deseq_extended_folder,
-                    comparison_file.replace(
-                        ".csv", "_with_annotation_and_countings.csv"
-                    ),
-                ),
-                "w",
+
+            output_fh = open(f"{self._deseq_extended_folder}/{comparison_file.replace('.csv', '_with_annotation_and_countings.csv')}", "w")
+            output_fh.write(
+                f"# Reference library (divisor): {combo[1]}\n"
             )
             output_fh.write(
-                "# Reference library (divisor): {}\n".format(combo[1])
-            )
-            output_fh.write(
-                "# Comparison library (numerator): {}\n".format(combo[0])
+                f"# Comparison library (numerator): {combo[0]}\n"
             )
             try:
                 deseq_result_fh = open(
-                    "%s/%s" % (self._deseq_raw_folder, comparison_file)
+                    f"{self._deseq_raw_folder}/{comparison_file}"
                 )
             except:
                 sys.stderr.write(
-                    "Apparently DESeq did not generate the "
-                    'file "%s". Extension stopped.\n' % comparison_file
+                    f"Apparently DESeq did not generate the "
+                    f"file '{comparison_file}'. Extension stopped.\n"
                 )
                 continue
             for counting_file_row, comparison_file_row in zip(
@@ -121,7 +108,7 @@ class DESeqRunner(object):
                     comparison_file_row = [""] + comparison_file_row
                     # Extend column description
                     counting_file_row[self._first_data_column :] = [
-                        "%s raw countings" % lib_name
+                        f"{lib_name} raw countings"
                         for lib_name in counting_file_row[
                             self._first_data_column :
                         ]
@@ -148,57 +135,58 @@ class DESeqRunner(object):
             cooks_cutoff_str = ", cooksCutoff=FALSE"
         for index, condition_combo in enumerate(condition_combos):
             call_string += (
-                "comp%s <- results(dds, contrast="
-                "c('condition','%s', '%s')%s)\n"
-                % (
-                    index,
-                    condition_combo[0],
-                    condition_combo[1],
-                    cooks_cutoff_str,
-                )
+                f"comp{index} <- results(dds, contrast="
+                f"c('condition','{condition_combo[0]}', '{condition_combo[1]}'){cooks_cutoff_str})\n"
             )
-            comparison_file = "deseq_comp_%s_vs_%s.csv" % (
-                condition_combo[0],
-                condition_combo[1],
-            )
+            comparison_file = f"deseq_comp_{condition_combo[0]}_vs_{condition_combo[1]}.csv"
             self._comparison_files_and_combos.append(
                 (comparison_file, list(condition_combo))
             )
             call_string += (
-                "write.table(comp%s, file='%s/%s', "
+                f"write.table(comp{index}, file='{self._deseq_raw_folder}/{comparison_file}', "
                 "quote=FALSE, sep='\\t')\n"
-                % (index, self._deseq_raw_folder, comparison_file)
             )
         return call_string
 
-    def _deseq_script_template(self):
+
+    #self._gene_wise_quanti_combined_path,
+    #self._first_data_column - 1,
+    #len(libs),
+    #self._first_data_column,
+    #libs_str,
+    #condition_str,
+    #beta_prior_str,
+    #self._deseq_pca_heatmap_path
+
+    def _deseq_script_template(self, libs_length, libs_str, condition_str, beta_prior_str):
         return (
-            "library('DESeq2')\n"
-            "rawCountTable <- read.table('%s', skip=1, sep='\\t', "
-            "quote='', comment.char='', "
-            "colClasses=c(rep('character',%s), rep('numeric',%s)))\n"
-            "countTable <- round(rawCountTable[,%s:length(names("
-            "rawCountTable))])\n"
-            "libs <- c(%s)\n"
-            "conds <- c(%s)\n"
-            "colnames(countTable) <- libs\n"
-            "samples <- data.frame(row.names=libs, condition=conds, "
-            "lib=libs)\n"
-            "dds <- DESeqDataSetFromMatrix(countData=countTable, "
-            "colData=samples, design=~condition)\n"
-            "dds <- DESeq(dds%s)\n\n"
-            "# PCA plot\n"
-            "pdf('%s')\n"
-            "rld <- rlog(dds)\n"
-            "print(plotPCA(rld, intgroup=c('condition')))\n\n"
-            "# Heatmap\n"
-            "library('RColorBrewer')\n"
-            "library('gplots')\n"
-            "distsRL <- dist(t(assay(rld)))\n"
-            "mat <- as.matrix(distsRL)\n"
-            "rownames(mat) <- with(colData(dds), "
-            "paste(lib, sep=' : '))\n"
-            "hmcol <- colorRampPalette(brewer.pal(9, 'GnBu'))(100)\n"
-            "heatmap.2(mat, trace='none', col = rev(hmcol), "
-            "margin=c(13, 13))\n"
+            f"library('DESeq2')\n"
+            f"rawCountTable <- read.table('{self._gene_wise_quanti_combined_path}', skip=1, sep='\\t', "
+            f"quote='', comment.char='', "
+            f"colClasses=c(rep('character',{self._first_data_column - 1}), rep('numeric',{libs_length})))\n"
+            f"countTable <- round(rawCountTable[,{self._first_data_column}:length(names("
+            f"rawCountTable))])\n"
+            f"libs <- c({libs_str})\n"
+            f"conds <- c({condition_str})\n"
+            f"colnames(countTable) <- libs\n"
+            f"samples <- data.frame(row.names=libs, condition=conds, "
+            f"lib=libs)\n"
+            f"head(samples)\n"
+            f"dds <- DESeqDataSetFromMatrix(countData=countTable, "
+            f"colData=samples, design=~condition)\n"
+            f"dds <- DESeq(dds{beta_prior_str})\n\n"
+            f"# PCA plot\n"
+            f"pdf('{self._deseq_pca_heatmap_path}')\n"
+            f"rld <- rlog(dds)\n"
+            f"print(plotPCA(rld, intgroup=c('condition')))\n\n"
+            f"# Heatmap\n"
+            f"library('RColorBrewer')\n"
+            f"library('gplots')\n"
+            f"distsRL <- dist(t(assay(rld)))\n"
+            f"mat <- as.matrix(distsRL)\n"
+            f"rownames(mat) <- with(colData(dds), "
+            f"paste(lib, sep=' : '))\n"
+            f"hmcol <- colorRampPalette(brewer.pal(9, 'GnBu'))(100)\n"
+            f"heatmap.2(mat, trace='none', col = rev(hmcol), "
+            f"margin=c(13, 13))\n"
         )
